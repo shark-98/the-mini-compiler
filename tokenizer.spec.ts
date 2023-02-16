dca32d9e1c17450da31947c8082640707e6170cf
likangbo <67063670@qq.com>
2023-02-17 01:34:06 +0800

    feat: add tokenizer

---
+++ tokenizer.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect } from "vitest";
+import { tokenizer, TokenTypes } from "./tokenizer";
+
+test("tokenizer", () => {
+  const code = `(add 2 (subtract 4 2))`;
+  const tokens = [
+    { type: TokenTypes.Paren, value: "(" },
+    { type: TokenTypes.Name, value: "add" },
+    { type: TokenTypes.Number, value: "2" },
+    { type: TokenTypes.Paren, value: "(" },
+    { type: TokenTypes.Name, value: "subtract" },
+    { type: TokenTypes.Number, value: "4" },
+    { type: TokenTypes.Number, value: "2" },
+    { type: TokenTypes.Paren, value: ")" },
+    { type: TokenTypes.Paren, value: ")" },
+  ];
+
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test("left paren", () => {
+  const code = `(`;
+  const tokens = [{ type: TokenTypes.Paren, value: "(" }];
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test("right paren", () => {
+  const code = `)`;
+  const tokens = [{ type: TokenTypes.Paren, value: ")" }];
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test("add", () => {
+  const code = `add`;
+  const tokens = [{ type: TokenTypes.Name, value: "add" }];
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+test("number", () => {
+  const code = `22`;
+  const tokens = [{ type: TokenTypes.Number, value: "22" }];
+  expect(tokenizer(code)).toEqual(tokens);
+});
+
+
+test("(add 1 2)", () => {
+  const code = `(add 1 2)`;
+  const tokens = [
+    { type: TokenTypes.Paren, value: "(" },
+    { type: TokenTypes.Name, value: "add" },
+    { type: TokenTypes.Number, value: "1" },
+    { type: TokenTypes.Number, value: "2" },
+    { type: TokenTypes.Paren, value: ")" },
+  ];
+
+  expect(tokenizer(code)).toEqual(tokens);
+});
---
+++ tokenizer.ts
@@ -0,0 +1,110 @@
+export enum TokenTypes {
+  Paren,
+  Name,
+  Number
+}
+interface Token {
+  type: TokenTypes,
+  value: string
+}
+type Tokens = Token[]
+
+export const LEFT_PAREN = '('
+export const RIGHT_PAREN = ')'
+const leftOrRightParen = [LEFT_PAREN, RIGHT_PAREN]
+const isLeftOrRightParen = (char: string): boolean => {
+  return leftOrRightParen.includes(char)
+}
+
+let current = 0
+const initCurrent = () => {
+  current = 0
+}
+const advanceCurrent = () => {
+  current++
+}
+
+let char = ''
+const initChar = () => {
+  char = ''
+}
+const setChar = (val: string) => {
+  char = val
+}
+
+const handleSpace = (): boolean => {
+  const SPACE_REGEX = /\s/
+
+  return SPACE_REGEX.test(char)
+}
+const handleLeftOrRightParen = (tokens: Tokens): boolean => {
+  if (!isLeftOrRightParen(char)) {
+    return false
+  }
+
+  tokens.push({
+    type: TokenTypes.Paren,
+    value: char
+  })
+  return true
+}
+
+const handleRegex = (regex: RegExp, code: string, sourceLength: number): string => {
+  let result = ''
+
+  if (regex.test(char)) {
+    while (current < sourceLength && regex.test(char)) {
+      result += char
+      advanceCurrent()
+      char = code[current]
+    }
+  }
+
+  return result
+}
+const handleName = (tokens: Tokens, code: string, sourceLength: number): boolean => {
+  const NAME_REGEX = /[a-z]/i
+  const value = handleRegex(NAME_REGEX, code, sourceLength)
+  if (value) {
+    tokens.push({
+      type: TokenTypes.Name,
+      value
+    })
+  }
+
+  return !!value
+}
+const handleNumber = (tokens: Tokens, code: string, sourceLength: number): boolean => {
+  const NUMBER_REGEX = /[0-9]/
+  const value = handleRegex(NUMBER_REGEX, code, sourceLength)
+  if (value) {
+    tokens.push({
+      type: TokenTypes.Number,
+      value
+    })
+  }
+
+  return !!value
+}
+
+export const tokenizer = (code: string): Tokens => {
+  const sourceLength = code.length
+  const tokens: Tokens = []
+  initCurrent()
+  initChar()
+
+  while (current < sourceLength) {
+    setChar(code[current])
+
+    if (handleSpace() || handleLeftOrRightParen(tokens)) {
+      advanceCurrent()
+      continue
+    }
+
+    if (handleName(tokens, code, sourceLength) || handleNumber(tokens, code, sourceLength)) {
+      continue
+    }
+  }
+
+  return tokens
+};
